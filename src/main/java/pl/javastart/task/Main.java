a3eb913e8f9824a8b99d15673e96a61251ead894
kamilkubiak2210 <kamil.kubiak4@outlook.com>
2023-03-27 17:51:14 +0200

    Poprawki

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -6,11 +6,11 @@ public class Main {
         String firstProduct = "Mleko";
         String firstProductProducer = "Mlekowita";
         double firstProductprice = 2.5;
-        String secondProductProduct = "Czekolada";
+        String secondProduct = "Czekolada";
         String secondProductProducer = "Wedel";
         double secondProductPrice = 2.19;
 
         System.out.println(firstProduct + " " + firstProductProducer + " " + firstProductprice + "zł");
-        System.out.println(secondProductProduct + " " + secondProductProducer + " " + secondProductPrice + "zł");
+        System.out.println(secondProduct + " " + secondProductProducer + " " + secondProductPrice + "zł");
     }
 }
